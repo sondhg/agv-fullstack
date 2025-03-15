070a56d6b78e4e9a5a6598758c347c311d72a198
sondhg <sondhw@gmail.com>
2025-03-15 17:01:33 +0700

    add map_data app structure and update PageMap component

--- frontend/src/app/admin/map/PageMap.tsx
+++ frontend/src/app/admin/map/PageMap.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export function PageMap() {
   return (
     <>
       <div className="space-y-5">
         <h2 className="text-3xl font-bold">Map</h2>
+        <Button>Import CSV to create map</Button>
       </div>
     </>
   );
