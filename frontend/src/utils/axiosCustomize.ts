0e0fee357e02721e56a4923344413aca16c179c6
sondhg <sondhw@gmail.com>
2025-03-26 22:44:46 +0700

    update frontend to use VITE_API_BASE_URL for API base URL configuration in axios

--- frontend/src/utils/axiosCustomize.ts
+++ frontend/src/utils/axiosCustomize.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../redux/store";
 
 const api = axios.create({
-  baseURL: "http://localhost:8001/api/",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/",
   headers: {
     "content-type": "application/json",
   },
