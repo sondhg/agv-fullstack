da2b444be8f78519461f18cfc5d11c9398e7e9a8
sondhg <sondhw@gmail.com>
2025-03-26 21:06:33 +0700

    add Docker support for backend with Dockerfile, docker-compose.yml, and environment configuration

--- agv_server/agv_server/settings.py
+++ agv_server/agv_server/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv('.env.local')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%_nvdij7!fp1u(5mr-m$b97tx+z2j#dbod$=lw7a8$uz-lkny1'
+# SECRET_KEY = 'django-insecure-%_nvdij7!fp1u(5mr-m$b97tx+z2j#dbod$=lw7a8$uz-lkny1'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.getenv("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "http://127.0.0.1").split(",")
 
 # Application definition
 
@@ -82,14 +90,25 @@ WSGI_APPLICATION = 'agv_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "agv_backend",  # Name of the database you created
+#         "USER": "postgres",  # PostgreSQL user you created
+#         "PASSWORD": "12345678",  # Password for the user
+#         "HOST": "localhost",  # Or the server address
+#         "PORT": "5432",  # Default PostgreSQL port
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "agv_backend",  # Name of the database you created
-        "USER": "postgres",  # PostgreSQL user you created
-        "PASSWORD": "12345678",  # Password for the user
-        "HOST": "localhost",  # Or the server address
-        "PORT": "5432",  # Default PostgreSQL port
+        "ENGINE": "django.db.backends.{}".format(os.getenv("DATABASE_ENGINE", "postgresql")),
+        "NAME": os.getenv("DATABASE_NAME", "agv_backend"),
+        "USER": os.getenv("DATABASE_USER", "postgres"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "12345678"),
+        "HOST": os.getenv("DATABASE_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DATABASE_PORT", "5432"),
     }
 }
 
@@ -129,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- frontend/src/utils/axiosCustomize.ts
+++ frontend/src/utils/axiosCustomize.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../redux/store";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "http://localhost:8001/api/",
   headers: {
     "content-type": "application/json",
   },
