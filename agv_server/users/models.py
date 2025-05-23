bbc37069af1d1836c7f2c9cc19a5f3c0caa50c48
sondhg <sondhw@gmail.com>
2025-05-23 14:42:20 +0700

    feat: implement custom user manager for user creation and superuser handling so when i run python manage.py runserver, no error will happen

--- agv_server/users/models.py
+++ agv_server/users/models.py
@@ -1,10 +1,28 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
 
 # làm theo https://youtu.be/PUzgZrS_piQ?si=-sonfq6T1UNCSbrt
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
 # Create your models here.
+
+
 class User(AbstractUser):
     name = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, unique=True)
@@ -14,3 +32,5 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
