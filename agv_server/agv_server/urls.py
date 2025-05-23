72a4e2ce1cf191d05318cf47c782e22b190b59b6
sondhg <sondhw@gmail.com>
2025-05-23 18:59:08 +0700

    fix: correct import order for admin site in URL configuration

--- agv_server/agv_server/urls.py
+++ agv_server/agv_server/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 from django.urls import include, path
-
+from django.contrib import admin
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("api/", include([
         path("auth/", include("users.urls")),
         path("orders/", include("order_data.urls")),
