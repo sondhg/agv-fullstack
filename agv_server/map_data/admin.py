1c4c7073289cd464a1de4d575a7d2170c736d854
sondhg <sondhw@gmail.com>
2025-05-23 14:59:15 +0700

    feat: register models in admin for Agv, MapData, Connection, Direction, Order, and User

--- agv_server/map_data/admin.py
+++ agv_server/map_data/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MapData, Connection, Direction
 
 # Register your models here.
+admin.site.register(MapData)
+admin.site.register(Connection)
+admin.site.register(Direction)
